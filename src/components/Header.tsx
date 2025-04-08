8c99590482a5362c1f9e8a97ebacfcc50fbde590
Sam-Otuonye Royal <samotuonyeroyal@gmail.com>
2025-04-08 13:28:02 +0100

    fixed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import Sun from '/assets/images/icon-sun.svg'
 import Moon from '/assets/images/icon-moon.svg'
+import Logo from '/assets/images/logo.svg'
 
 type HeaderProps={
      darkMode:boolean
@@ -12,7 +13,7 @@ function Header(props:HeaderProps) {
     <header>
           <div className={`container mx-auto px-4 py-6 md:px-0 ${props.darkMode ? 'dark' : ''}`}>
                <div className="flex items-center justify-between bg-neutral-0 dark:bg-neutral-700 p-4 rounded-lg shadow-2xl">
-                    <img src="/assets/images/logo.svg" alt="logo" />
+                    <img src={Logo} alt="logo" />
                     <button className="bg-neutral-300 dark:bg-neutral-500 p-2 rounded-lg cursor-pointer" onClick={props.toggleDarkMode}><img src={props.darkMode ? Sun: Moon} alt="switch to dark mode" /></button>
                </div>
           </div>
